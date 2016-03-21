42984a66b7d1afd8b4f6c0aa1f6e1f3b862c8499
Akvarel41 <elenkasemenova@mail.ru>
2016-03-21 14:11:09 +0300

    Проверка правильности перемещения кнопок

---
+++ AutoMoveJUnitTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import akvarel.Spektr;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author км
+ */
+public class AutoMoveJUnitTest extends TestCase{
+    private Spektr spektr;
+    
+    //Before
+    public void setUp() {
+        spektr = new Spektr();
+    }
+    
+    //After
+    public void tearDown() {
+        spektr = null;
+    }
+    
+     public void testMove()
+     {
+         int[][] NewNumbers = new int[5][5];
+         for (int i = 0; i < 5; i++) 
+         {
+            for (int j = 0; j < 5; j++)
+            {
+                spektr.numbers[i][j] = i * 5 + j;
+                NewNumbers[i][j] = i * 5 + j;
+            }
+         }         
+         NewNumbers[0][0] = NewNumbers[1][0];         
+         NewNumbers[1][0] = 0;
+         spektr.move(1, 0, spektr.numbers[1][0]);//ноль в строке сверху
+         for (int i = 0; i < 5; i++)
+             for(int j = 0; j < 5; j ++)
+             {
+                 assertEquals( spektr.numbers[i][j] , NewNumbers[i][j]);
+             }
+         
+         NewNumbers[1][0] = NewNumbers[0][0];         
+         NewNumbers[0][0] = 0;
+         spektr.move(0, 0, spektr.numbers[0][0]);//ноль в строке снизу
+         for (int i = 0; i < 5; i++)
+             for(int j = 0; j < 5; j ++)
+             {
+                 assertEquals( spektr.numbers[i][j] , NewNumbers[i][j]);
+             }
+         NewNumbers[0][0] = NewNumbers[0][1];         
+         NewNumbers[0][1] = 0;
+         spektr.move(0, 1, spektr.numbers[0][1]);//ноль в строке сверху
+         for (int i = 0; i < 5; i++)
+             for(int j = 0; j < 5; j ++)
+             {
+                 assertEquals( spektr.numbers[i][j] , NewNumbers[i][j]);
+             }
+         NewNumbers[0][1] = NewNumbers[0][0];         
+         NewNumbers[0][0] = 0;
+         spektr.move(0, 0, spektr.numbers[0][0]);//ноль в строке сверху
+         for (int i = 0; i < 5; i++)
+             for(int j = 0; j < 5; j ++)
+             {
+                 assertEquals( spektr.numbers[i][j] , NewNumbers[i][j]);
+             }
+     }
+
+}
